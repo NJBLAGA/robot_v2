b8fd408167d93b1611847a237b44b80ed4fa1822
NJBLAGA <nathanblaga90@gmail.com>
2021-08-19 23:03:33 +1000

    add Board class tests for -- create,update, place and display methods

--- lib/robot_v2/board.rb
+++ lib/robot_v2/board.rb
@@ -5,29 +5,29 @@ module RobotV2
     attr_accessor :board
 
     def initialization
-      @table = []
+      @board = []
       @prev_position_x = 0
       @prev_position_y = 0
     end
 
     def create_board(size)
-      @table = Array.new(size) { Array.new(size, 'X') }
+      @board = Array.new(size) { Array.new(size, 'X') }
     end
 
     def draw_board
       puts "\n"
-      puts @table.reverse.map { |x| x.join(' ') }
+      puts @board.reverse.map { |x| x.join(' ') }
       puts "\n"
     end
 
     def update_last_position
-      @table[@prev_position_x.to_i][@prev_position_y.to_i] = 'X'
+      @board[@prev_position_x.to_i][@prev_position_y.to_i] = 'X'
     end
 
     def place_robot(position_x, position_y)
-      @table[position_x][position_y] = 'R'
       @prev_position_x = position_x
       @prev_position_y = position_y
+      @board[position_x][position_y] = 'R'
     end
 
     def display_board(position_x, position_y)
