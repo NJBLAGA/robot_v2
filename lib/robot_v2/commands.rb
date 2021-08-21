605ddd32659a9f49ee49132b25feb6989c7cf1a4
NJBLAGA <nathanblaga90@gmail.com>
2021-08-21 22:31:40 +1000

    add Command methods -- turn_left, turn_right

--- lib/robot_v2/commands.rb
+++ lib/robot_v2/commands.rb
@@ -9,6 +9,7 @@ module RobotV2
       @robot = RobotV2::Robot.new
       @rendered_robot = {}
       @rendered_board = nil
+      @robot_placed = false
     end
 
     def create_new_board(size)
@@ -29,17 +30,88 @@ module RobotV2
       @rendered_board[position_x][position_y].include?('R') == true)
     end
 
-    def valid_placement
-      position_x = @rendered_robot[:position][0].to_i
-      position_y = @rendered_robot[:position][1].to_i
+    def valid_placement(new_x_position, new_y_position, new_direction)
+      @position_x = new_x_position
+      @position_y = new_y_position
       begin
-        if negative_input(position_x, position_y) == true && valid_tile(position_x, position_y) == true
-          @robot.robot_report
+        if negative_input(@position_x, @position_y) == true && valid_tile(@position_x, @position_y) == true
+          update_robot_position(new_x_position, new_y_position, new_direction)
+          @robot_placed = true
         else
-          puts "Invalid Input -- X & Y must be positive and exist within the board's dimensions -- Try Again"
+          puts 'Invalid Command -- Try Again'
         end
       rescue 
-        puts "Inputs of #{@rendered_robot[:position]} are outside the board's dimensions -- Try Again"
+        puts 'Invalid Command -- Try Again'
+      end
+    end
+
+    def move_robot
+      case @rendered_robot[:direction]
+      when 'NORTH'
+        @rendered_robot[:position][0] += 1
+      when 'EAST'
+        @rendered_robot[:position][1] += 1
+      when 'SOUTH'
+        @rendered_robot[:position][0] -= 1
+      when 'WEST'
+        @rendered_robot[:position][1] -= 1
+      end
+      check_move
+    end
+
+    def revert_robot
+      case @rendered_robot[:direction]
+      when 'NORTH'
+        @rendered_robot[:position][0] -= 1
+      when 'EAST'
+        @rendered_robot[:position][1] -= 1
+      when 'SOUTH'
+        @rendered_robot[:position][0] += 1
+      when 'WEST'
+        @rendered_robot[:position][1] += 1
+      end
+      puts 'ABORT MOVE -- Action would cuase Robot to fall off the board. -- TRY AGAIN'
+    end
+
+    def check_move
+      if valid_placement(@rendered_robot[:position][0], @rendered_robot[:position][1], @rendered_robot[:direction]) == true
+      else
+        revert_robot
+      end
+    end
+
+    def turn_left
+      case @rendered_robot[:direction]
+      when 'NORTH'
+        @rendered_robot[:direction] = 'WEST'
+      when 'EAST'
+        @rendered_robot[:direction] = 'NORTH'
+      when 'SOUTH'
+        @rendered_robot[:direction] = 'EAST'
+      when 'WEST'
+        @rendered_robot[:direction] = 'SOUTH'
+      end
+      update_robot_position(@rendered_robot[:position][0], @rendered_robot[:position][1], @rendered_robot[:direction])
+    end
+
+    def turn_right
+      case @rendered_robot[:direction]
+      when 'NORTH'
+        @rendered_robot[:direction] = 'EAST'
+      when 'EAST'
+        @rendered_robot[:direction] = 'SOUTH'
+      when 'SOUTH'
+        @rendered_robot[:direction] = 'WEST'
+      when 'WEST'
+        @rendered_robot[:direction] = 'NORTH'
+      end
+      update_robot_position(@rendered_robot[:position][0], @rendered_robot[:position][1], @rendered_robot[:direction])
+    end
+
+    def report_position
+      if @robot_placed == true
+        @board.display_board(@rendered_robot[:position][0], @rendered_robot[:position][1])
+        @robot.robot_report
       end
     end
   end
